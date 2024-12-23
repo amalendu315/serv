28a553f842c0676231cb002de116721d46b9050e
Amalendu Pandey <amalendupandey56@gmail.com>
2024-12-23 12:11:26 +0530

    backedd

--- src/controllers/flightops/akasa.ts
+++ src/controllers/flightops/akasa.ts
@@ -158,7 +158,7 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
 
                      const OldPur = JSON.stringify(record.Pur);
                      const DepinputTime = record.Dep;
-                     const date = moment.utc(DepinputTime).tz("Asia/Kolkata");
+                const date = moment.utc(DepinputTime).tz("Asia/Kolkata");
                      const minute = date.minutes();
                      const lastDigit = minute % 10;
                      let roundedMinutes = 0;
@@ -166,13 +166,7 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
                        ? (roundedMinutes = minute)
                        : (roundedMinutes = minute + 1);
                      date.minutes(roundedMinutes);
-                     const OldDep =
-                       environment === "development"
-                         ? date.format("HH:mm A")
-                         : moment(date)
-                             .subtract(5, "hours")
-                             .subtract(30, "minutes")
-                             .format("HH:mm A");
+                     const OldDep = date.format("HH:mm A");
                      const ArrinputTime = record.Arr;
                      const dateb = moment.utc(ArrinputTime).tz("Asia/Kolkata");
                      const minuteb = dateb.minutes();
@@ -182,13 +176,7 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
                        ? (roundedMinutesb = minuteb)
                        : (roundedMinutesb = minuteb + 1);
                      dateb.minutes(roundedMinutesb);
-                     const OldArr =
-                       environment === "development"
-                         ? dateb.format("HH:mm A")
-                         : moment(dateb)
-                             .subtract(5, "hours")
-                             .subtract(30, "minutes")
-                             .format("HH:mm A");
+                     const OldArr = dateb.format("HH:mm A");
 
                      const OldDate = moment(record.TravelDate).format(
                        "YYYY-MM-DD"
@@ -208,42 +196,7 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
                      };
 
                      const MyRemarks = CheckStatus();
-                     if(environment === "production"){
-                      const result =
-                        record.PNR +
-                        "|" +
-                        Origin +
-                        "|" +
-                        Destination +
-                        "|" +
-                        record.Flight +
-                        "|" +
-                        flightNumber +
-                        "|" +
-                        OldPur +
-                        "|" +
-                        PAX +
-                        "|" +
-                        OldDate +
-                        "|" +
-                        depDate +
-                        "|" +
-                        OldDep +
-                        "|" +
-                        depTime +
-                        "|" +
-                        OldArr +
-                        "|" +
-                        arrTime +
-                        "|" +
-                        MyRemarks;
-
-                        return {
-                          pnr: record.PNR,
-                          data: result,
-                        };
-
-                     } else {
+                     
                       const result =
                         record.PNR +
                         "|" +
@@ -276,7 +229,6 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
                            pnr: record.PNR,
                            data: result,
                          };
-                     }
                     
                    } else {
                      const PNR = bookingData.recordLocator;
@@ -320,209 +272,3 @@ export const getAkasaStatus = async (req:Request, res:Response) => {
         res.status(500).send(error);
     }
 };
-
-// export const getAkasaStatus = async (req: Request, res: Response) => {
-//   try {
-//     const data = await makeApiCall(10);
-//     const myToken = data?.data?.token;
-//     const header = `Bearer ${myToken}`;
-
-//     const fullName = req.file?.filename;
-//     if (!fullName) {
-//       throw new Error("No file uploaded");
-//     }
-
-//     const wb = xlsx.readFile("./uploads/" + fullName, { cellDates: true });
-//     const ws = wb.Sheets["Sheet1"];
-//     ws["!ref"] = "A1:K3000";
-//     const jsonSheet = xlsx.utils.sheet_to_json(ws);
-
-//     const chunkSize = 5;
-//     const allResults: [] = [];
-
-//     // Use Bluebird's map function with concurrency option
-//     await bluebird.map(
-//       jsonSheet,
-//       async (record: any, index: number) => {
-//         // Introduce a small delay to avoid overwhelming the API
-//         await bluebird.delay(index * 100); // 100ms delay per request
-
-//         const PNR = record?.PNR;
-
-//         const config1 = {
-//           method: "get",
-//           url: `${akasaPnrRetrieveUrl}?recordLocator=${PNR}&emailAddress=Airlines@Airiq.In`,
-//           headers: {
-//             Authorization: header,
-//             "Content-Type": "application/json",
-//             "User-Agent":
-//               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-//           },
-//           httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-//         };
-//         const config2 = {
-//           method: "get",
-//           url: `${akasaPnrRetrieveUrl}?recordLocator=${PNR}&emailAddress=airlinesairiq@gmail.com`,
-//           headers: {
-//             Authorization: header,
-//             "Content-Type": "application/json",
-//             "User-Agent":
-//               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-//           },
-//           httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-//         };
-
-//         let response: AxiosResponse<any>;
-//         try {
-//           response = await axios(config1);
-//         } catch (error) {
-//           try {
-//             response = await axios(config2);
-//           } catch (error: any) {
-//             return {
-//               pnr: record.PNR,
-//               error: `Error processing PNR ${PNR}: ${error?.message}`,
-//             };
-//           }
-//         }
-
-//         try {
-//           if (!response || !response.data || !response.data.data) {
-//             throw new Error("Invalid API response");
-//           }
-
-//           const bookingData = response.data.data;
-//           const journeys = bookingData.journeys;
-//           const length = journeys?.length;
-
-//           if (length > 0) {
-//             const Destination = bookingData?.journeys[0].designator.destination;
-//             const Origin = bookingData.journeys[0].designator.origin;
-//             const depDate = bookingData.journeys[0].designator.arrival.slice(
-//               0,
-//               10
-//             );
-//             const arrTime = checkTimeFormat(
-//               bookingData.journeys[0].designator.arrival.slice(11, 16)
-//             );
-//             const depTime = checkTimeFormat(
-//               bookingData.journeys[0].designator.departure.slice(11, 16)
-//             );
-//             const PAX = Object.keys(bookingData.breakdown.passengers).length;
-//             const flightReference =
-//               bookingData.journeys[0].segments[0].flightReference;
-//             const qpCodeRegex = /QP(\d+)/;
-//             const match = flightReference.match(qpCodeRegex);
-//             const flightNumber = match ? match[1] : null;
-
-//             const OldPur = JSON.stringify(record.Pur);
-//             const DepinputTime = record.Dep;
-//             const date = moment.utc(DepinputTime).tz("Asia/Kolkata");
-//             const minute = date.minutes();
-//             const lastDigit = minute % 10;
-//             let roundedMinutes = 0;
-//             lastDigit === 0 || lastDigit === 5
-//               ? (roundedMinutes = minute)
-//               : (roundedMinutes = minute + 1);
-//             date.minutes(roundedMinutes);
-//             const OldDep = date.format("HH:mm A");
-//             const ArrinputTime = record.Arr;
-//             const dateb = moment.utc(ArrinputTime).tz("Asia/Kolkata");
-//             const minuteb = dateb.minutes();
-//             const lastDigitb = minuteb % 10;
-//             let roundedMinutesb = 0;
-//             lastDigitb === 0 || lastDigitb === 5
-//               ? (roundedMinutesb = minuteb)
-//               : (roundedMinutesb = minuteb + 1);
-//             dateb.minutes(roundedMinutesb);
-//             const OldArr = dateb.format("HH:mm A");
-
-//             const OldDate = moment(record.TravelDate).format("YYYY-MM-DD"); //Date
-//             const CheckStatus = () => {
-//               if (
-//                 record.Flight != flightNumber ||
-//                 OldPur != JSON.stringify(PAX) ||
-//                 OldDate != depDate ||
-//                 OldDep != depTime ||
-//                 OldArr != arrTime
-//               ) {
-//                 return "BAD";
-//               } else {
-//                 return "GOOD";
-//               }
-//             };
-
-//             const MyRemarks = CheckStatus();
-//             const result =
-//               record.PNR +
-//               "|" +
-//               Origin +
-//               "|" +
-//               Destination +
-//               "|" +
-//               record.Flight +
-//               "|" +
-//               flightNumber +
-//               "|" +
-//               OldPur +
-//               "|" +
-//               PAX +
-//               "|" +
-//               OldDate +
-//               "|" +
-//               depDate +
-//               "|" +
-//               OldDep +
-//               "|" +
-//               depTime +
-//               "|" +
-//               OldArr +
-//               "|" +
-//               arrTime +
-//               "|" +
-//               MyRemarks;
-//             return {
-//               pnr: record.PNR,
-//               data: result,
-//             };
-//           } else {
-//             const PNR = bookingData.recordLocator;
-//             return {
-//               pnr: PNR,
-//               data: `${PNR} is Cancelled`,
-//             };
-//           }
-//         } catch (error: any) {
-//           if (error?.response?.status === 404) {
-//             return {
-//               pnr: record.PNR,
-//               error: `PNR NOT FOUND ${record.PNR}`,
-//             };
-//           } else {
-//             return {
-//               pnr: record.PNR,
-//               error: `Error processing PNR ${record.PNR}: ${error?.message}`,
-//             };
-//           }
-//         }
-//       },
-//       { concurrency: chunkSize }
-//     ); // Control concurrency with Bluebird
-
-//     const { results, errors } = allResults.reduce(
-//       (acc, result: any) => {
-//         if (result?.error) {
-//           acc.errors.push(result?.error);
-//         } else if (result.data) {
-//           acc.results.push(result.data); // Accumulate results as an array
-//         }
-//         return acc;
-//       },
-//       { results: [] as string[], errors: [] as string[] }
-//     );
-
-//     res.status(200).send({ results, errors });
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// };
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -335,11 +335,9 @@ function transformData(
             "Warning: 'DOB (DD/MM/YYYY)' property not found in record."
           );
         } else {
-          const dobDate = new Date(record["DOB (DD/MM/YYYY)"]); // Create a Date object
-          const day = dobDate.getDate().toString().padStart(2, "0");
-          const month = (dobDate.getMonth() + 1).toString().padStart(2, "0"); // Month is 0-indexed
-          const year = dobDate.getFullYear();
-          record["DOB (DD/MM/YYYY)"] = `${day}/${month}/${year}`;
+          const dobDate =record["DOB (DD/MM/YYYY)"]; // Create a Date object
+          
+          record["DOB (DD/MM/YYYY)"] = dobDate;
         }
 
         record["CITIZENSHIP"] = "INDIAN-IN";
